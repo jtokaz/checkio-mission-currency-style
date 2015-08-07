c090866da7d921c1d453e43ff54bc20b32c43a58
jtokaz <jtokaz@gmail.com>
2015-08-06 21:47:05 -0500

    Update tests.py

--- verification/tests.py
+++ verification/tests.py
@@ -10,58 +10,58 @@ Each test is dict with
 TESTS = {
     "Basics": [
         {
-            "input": "$5.34"
-            "answer": "$5.34",
-        },
-        {
-            "input": "$5,34"
+            "input": "$5.34",
             "answer": "$5.34"
         },
         {
-            "input": "$222,100,455.34"
-            "answer": "$222,100,455.34",
+            "input": "$5,34",
+            "answer": "$5.34"
         },
         {
-            "input": "$222.100.455,34"
-            "answer": "$222,100,455.34",
+            "input": "$222,100,455.34",
+            "answer": "$222,100,455.34"
+        },
+        {
+            "input": "$222.100.455,34",
+            "answer": "$222,100,455.34"
         }, 
         {
-            "input": "$222,100,455"
-            "answer": "$222,100,455",
+            "input": "$222,100,455",
+            "answer": "$222,100,455"
         }, 
         {
-            "input": "$222.100.455"
-            "answer": "$222,100,455",
-        },          
+            "input": "$222.100.455",
+            "answer": "$222,100,455"
+        }          
     ],
     "Extra": [
         {
             "input": "$4,13 + $5,24 = $9,37",
-            "answer": "$4.13 + $5.24 = $9.37",
+            "answer": "$4.13 + $5.24 = $9.37"
         },
         {
             "input": "$4,13 + $1.005,24 = $1.009,37",
-            "answer": "$4.13 + $1,005.24 = $1,009.37",
+            "answer": "$4.13 + $1,005.24 = $1,009.37"
         },
         {
             "input": "$8.000 - $8.000 = $0",
-            "answer": "$8,000 - $8,000 = $0",
+            "answer": "$8,000 - $8,000 = $0"
         },
         {
             "input": "$4.545,45 is less than $5,454.54.",
-            "answer": "$4,545.45 is less than $5,454.54.",
+            "answer": "$4,545.45 is less than $5,454.54."
         },
         {
             "input": "$4,545.45 is less than $5.454,54.",
-            "answer": "$4,545.45 is less than $5,454.54.",
+            "answer": "$4,545.45 is less than $5,454.54."
         },
         {
             "input": "Our movie tickets cost $12,20.",
-            "answer": "Our movie tickets cost $12.20.",
+            "answer": "Our movie tickets cost $12.20."
         },     
         {
             "input": "127.255.255.255",
-            "answer": "127.255.255.255",
+            "answer": "127.255.255.255"
         },
         {
             "input": ("Clayton Kershaw $31.000.000\n"
@@ -69,7 +69,7 @@ TESTS = {
                     "Adrian Gonzalez $21.857.143\n"),
             "answer": ("Clayton Kershaw $31,000,000\n"
                     "Zack Greinker   $27,000,000\n"
-                    "Adrian Gonzalez $21,857,143\n"),
-        },        
+                    "Adrian Gonzalez $21,857,143\n")
+        }        
     ]
 }
