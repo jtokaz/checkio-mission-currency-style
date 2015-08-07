2daded86041d34be04fc77eb1963f03c6941b771
jtokaz <jtokaz@gmail.com>
2015-08-06 22:02:30 -0500

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -38,10 +38,10 @@ api.add_listener(
     ON_CONNECT,
     CheckiOReferee(
         tests=TESTS,
-        cover_code={
-            'python-27': cover,
-            'python-3': cover
-        },
+        #cover_code={
+        #    'python-27': cover,
+        #    'python-3': cover
+        #},
         # checker=None,  # checkers.float.comparison(2)
         # add_allowed_modules=[],
         # add_close_builtins=[],
